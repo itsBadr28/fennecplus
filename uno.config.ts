59b8c0408d2b3e8f7e50a1fb30b085ec2a76321c
itsBadr28 <badreddine.ad@univ-constantine2.dz>
2024-12-15 16:25:32 +0100

    add fonts

--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,45 @@
-import { defineConfig } from 'unocss'
+import {
+  defineConfig,
+  presetAttributify,
+  presetUno,
+  presetIcons,
+} from 'unocss';
+import { presetTypography } from '@unocss/preset-typography';
 
-export default defineConfig({
-  // ...UnoCSS options
-})
+export function createConfig({ strict = true, dev = true } = {}) {
+  return defineConfig({
+    envMode: dev ? 'dev' : 'build',
+    theme: {
+      fontFamily: {
+        sans: "'Inter', sans-serif",
+        mono: "'Fira Code', monospace",
+        zain: ['"Zain"', 'sans-serif'],
+        vip: ['"VIP"', 'sans-serif'],
+        cairo: ['"cairo"', 'sans-serif'],
+        lemon: ['"lemon"', 'sans-serif'],
+      },
+      breakpoints: {
+        sm: '320px',
+        md: '912px',
+        lg: '1360px',
+      },
+      colors: {
+        main: '#505443',
+        secondary: '#7F8860',
+        accent: '#6A7057',
+        grey: {
+          light: '#EFEEEE',
+          dark: '#858282',
+        },
+      },
+    },
+    presets: [
+      presetAttributify({ strict }),
+      presetUno(),
+      presetIcons(),
+      presetTypography(),
+    ],
+  });
+}
+
+export default createConfig();
