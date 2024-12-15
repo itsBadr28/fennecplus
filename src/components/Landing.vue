97b384b8b26c901172702a71d38d9124616c4e78
itsBadr28 <badreddine.ad@univ-constantine2.dz>
2024-12-15 16:27:16 +0100

    add guest layout and landing page

---
+++ src/components/Landing.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="bg-black wscreen h90vh flex" >
+        <div class="flex w80% ma">
+            <div class="flex w90% ma">
+               <div class="w4/7 flex">
+                    <img src="../assets/hero.png" class="w90% mta  mla" alt="">
+                </div>
+            <div class=" w3/7 flex" >
+                <div class="mta mla" dir="rtl">
+                    <h2  class=" font-vip text-6xl text-white">عـــش الـتــجـربـة <br> و إستمتع بالمغامرة</h2>
+                    <h3 class="my4 text-2xl font-cairo">
+                        اكتشف تجربة IPTV الجزائرية المثالية مع  Fennec Plus<br> ابتداء من 500 دج/شهر فقط 
+                    </h3>
+                    <button class="bg-#96c834 py4 px4 w1/2 my3">
+                        <h2 class="text-3xl text-black uppercase font-lemon">
+                            get now
+                        </h2>
+                    </button>
+                </div>
+            </div>  
+            </div>
+             
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
