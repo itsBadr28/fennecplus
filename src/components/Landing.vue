ce67e98cc1ce5f7abba39513d614d96bd3e2880f
itsBadr28 <badreddine.ad@univ-constantine2.dz>
2024-12-16 18:20:41 +0100

    refactor Landing component layout for improved responsiveness and styling

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -2,20 +2,20 @@
 </script>
 
 <template>
-  <div class="bg-black wscreen h90vh flex" >
-        <div class="flex w80% ma">
-            <div class="flex w90% ma">
-               <div class="w4/7 flex">
-                    <img src="../assets/hero.png" class="w90% mta  mla" alt="">
+  <div class=" wscreen h90vh flex" >
+        <div class="flex wfull ma">
+            <div class="flex w100% ma">
+               <div class="w1/2 flex">
+                    <img src="../assets/hero.png" class="wfull mta  mla" alt="">
                 </div>
-            <div class=" w3/7 flex" >
+            <div class=" w1/2 flex" >
                 <div class="mta mla" dir="rtl">
-                    <h2  class=" font-vip text-6xl text-white">عـــش الـتــجـربـة <br> و إستمتع بالمغامرة</h2>
-                    <h3 class="my4 text-2xl font-cairo">
+                    <h2  class=" font-vip my5 text-6xl line-height-15 text-white">عـــش الـتــجـربـة <br> و إستمتع بالمغامرة</h2>
+                    <h3 class="m0 mb4 text-2xl font-cairo">
                         اكتشف تجربة IPTV الجزائرية المثالية مع  Fennec Plus<br> ابتداء من 500 دج/شهر فقط 
                     </h3>
-                    <button class="bg-#96c834 py4 px4 w1/2 my3">
-                        <h2 class="text-3xl text-black uppercase font-lemon">
+                    <button class="bg-#96c834 w3/5 my3">
+                        <h2 class="text-3xl text-black uppercase my2 font-lemon">
                             get now
                         </h2>
                     </button>
