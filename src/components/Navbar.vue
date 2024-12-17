a0f881967fa8122f07ae6211e3d28f3cd1fb9de1
YASMINEKEDDD <keddache.yasmine98@gmail.com>
2024-12-17 05:33:21 +0000

    the shadow button

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,16 +1,16 @@
 <template>
     <nav class="fixed top-0 left-0 w-full h-16 bg-black mx-auto font-mono font-bold">
-      <div class="flex justify-between items-center w-full h-full px-8"> <!-- Increased padding for better spacing -->
-        <!-- Logo on the left with increased margin -->
-        <div class="ml-20 mr-8"> <!-- Added margin-left and margin-right for logo -->
+      <div class="flex justify-between items-center w-full h-full px-8">
+        
+        <!-- Logo on the left with increased size and margin -->
+        <div class="ml-20 mr-8">
           <router-link to="/">
-            <!-- Utilisation du chemin relatif -->
-            <img src="../assets/logo--fennec--plus-04.png" alt="Logo" class="h-12" />
+            <img src="../assets/logo--fennec--plus-04.png" alt="Logo" class="h-18 w-auto" /> <!-- Increased logo size -->
           </router-link>
         </div>
           
         <!-- Navigation links aligned to the right -->
-        <ul class="flex items-center space-x-8 ml-auto list-none"> <!-- Increased space between links -->
+        <ul class="flex items-center space-x-8 ml-auto list-none">
           <li>
             <router-link to="/" class="text-white text-lg hover:underline">Home</router-link>
           </li>
@@ -21,10 +21,11 @@
             <router-link to="/contact" class="text-white text-lg hover:underline">Contact</router-link>
           </li>
         </ul>
-    
-        <!-- Button with increased space -->
-        <div class="ml-20 mr-40"> <!-- Increased margin for more space between button and navbar -->
-          <button class="bg-[rgb(173,255,47)] text-black px-6 py-2 text-lg rounded-lg font-extrabold hover:opacity-80">
+      
+        <!-- Button with brighter and shinier shadow -->
+        <div class="ml-20 mr-40">
+          <button
+            class="bg-[rgb(173,255,47)] text-black px-6 py-2 text-lg rounded-lg font-extrabold shadow-[0_0_15px_5px_rgb(173,255,47)] hover:shadow-[0_0_25px_10px_rgb(173,255,47)] hover:scale-110 transition-all duration-300">
             Get Now
           </button>
         </div>
@@ -37,8 +38,4 @@
     name: 'Navbar',
   };
   </script>
-  
-  <style scoped>
-  /* No additional styles needed, using UnoCSS classes */
-  </style>
   
