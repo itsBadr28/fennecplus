d424eb27e58229ef7d5bd1abd83491bab45f797e
YASMINEKEDDD <keddache.yasmine98@gmail.com>
2024-12-17 10:52:29 +0000

    edit landing and navbar

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-  <div class=" wscreen h90vh flex" >
+  <div class=" wscreen hscreen flex" >
         <div class="flex wfull ma">
             <div class="flex w90% ma">
                <div class="w55% flex">
@@ -11,11 +11,11 @@
             <div class=" w45% flex" >
                 <div class="mta mla" dir="rtl">
                     <h2  class=" font-vip my4 text-4rem line-height-4.5rem text-white">عـــش الـتــجـربـة <br> و إستمتع بالمغامرة</h2>
-                    <h3 class="m0 mb4 text-2xl font-cairo">
+                    <h3 class="m0 mb4 text-2xl  text-white font-cairo">
                         اكتشف تجربة IPTV الجزائرية المثالية مع  Fennec Plus<br> ابتداء من 500 دج/شهر فقط 
                     </h3>
                     <button class="bg-#96c834 w3/5 my3">
-                        <h2 class="text-3xl text-black uppercase my2 font-lemon">
+                        <h2 class="text-3xl text-black uppercase my2 font-lemon ">
                             get now
                         </h2>
                     </button>
