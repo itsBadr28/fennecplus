cf2c6b9b50eb0d3aca6815b45832b6540604d142
YASMINEKEDDD <107432486+YASMINEKEDDD@users.noreply.github.com>
2024-12-17 13:50:31 +0000

    5 download section (#7)
    * download images
    
    * add footer page
    
    * edit landing and navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="fixed top-0 left-0 w-full h-16 bg-black mx-auto font-mono font-bold">
+    <nav class="fixed top-0 left-0 w-full h-25 bg-black bg-opacity-50 mx-auto font-mono font-bold">
       <div class="flex justify-between items-center w-full h-full px-8">
         
         <!-- Logo on the left with increased size and margin -->
@@ -12,20 +12,20 @@
         <!-- Navigation links aligned to the right -->
         <ul class="flex items-center space-x-8 ml-auto list-none">
           <li>
-            <router-link to="/" class="text-white text-lg hover:underline">Home</router-link>
+            <router-link to="/" class="text-white text-lg font-semibold font-lemon hover:underline">Home</router-link>
           </li>
           <li>
-            <router-link to="/about" class="text-white text-lg hover:underline">About</router-link>
+            <router-link to="/about" class="text-white text-lg font-semibold font-lemon hover:underline">About</router-link>
           </li>
           <li>
-            <router-link to="/contact" class="text-white text-lg hover:underline">Contact</router-link>
+            <router-link to="/contact" class="text-white text-lg font-semibold font-lemon hover:underline">Contact</router-link>
           </li>
         </ul>
       
         <!-- Button with brighter and shinier shadow -->
         <div class="ml-20 mr-40">
           <button
-            class="bg-[rgb(173,255,47)] text-black px-6 py-2 text-lg rounded-lg font-extrabold shadow-[0_0_15px_5px_rgb(173,255,47)] hover:shadow-[0_0_25px_10px_rgb(173,255,47)] hover:scale-110 transition-all duration-300">
+            class="bg-[rgb(173,255,47)] text-black px-6 py-2 text-lg rounded-lg font-lemon shadow-[0_0_15px_5px_rgb(173,255,47)] hover:shadow-[0_0_25px_10px_rgb(173,255,47)] hover:scale-110 transition-all duration-300">
             Get Now
           </button>
         </div>
