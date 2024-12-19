7012de617aa23a073e4ed68fdc56be24790aa7c4
YASMINEKEDDD <keddache.yasmine98@gmail.com>
2024-12-19 09:17:54 +0000

    delete coment

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,14 +2,14 @@
     <nav class="fixed top-0 left-0 w-full h-25 bg-black bg-opacity-50 mx-auto font-mono font-bold">
       <div class="flex justify-between items-center w-full h-full px-8">
         
-        <!-- Logo on the left with increased size and margin -->
+       
         <div class="ml-20 mr-8">
           <router-link to="/">
-            <img src="../assets/logo--fennec--plus-04.png" alt="Logo" class="h-18 w-auto" /> <!-- Increased logo size -->
+            <img src="../assets/logo--fennec--plus-04.png" alt="Logo" class="h-18 w-auto" />
           </router-link>
         </div>
           
-        <!-- Navigation links aligned to the right -->
+        
         <ul class="flex items-center space-x-8 ml-auto list-none">
           <li>
             <router-link to="/" class="text-white text-lg font-semibold font-lemon hover:underline">Home</router-link>
@@ -22,7 +22,7 @@
           </li>
         </ul>
       
-        <!-- Button with brighter and shinier shadow -->
+        
         <div class="ml-20 mr-40">
           <button
             class="bg-[rgb(173,255,47)] text-black px-6 py-2 text-lg rounded-lg font-lemon shadow-[0_0_15px_5px_rgb(173,255,47)] hover:shadow-[0_0_25px_10px_rgb(173,255,47)] hover:scale-110 transition-all duration-300">
@@ -33,9 +33,6 @@
     </nav>
   </template>
   
-  <script>
-  export default {
-    name: 'Navbar',
-  };
+  <script setup lang="ts">
   </script>
   
